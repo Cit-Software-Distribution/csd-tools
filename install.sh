84f5b2f4ea49ffb6070bd495744d70942de638a7
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-06-23 18:08:42 -0300

    add: procurar por pacotes no manifesto

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 ARCH=$(uname -m)
 OS=$(uname -s)
 BIN_DIR="$HOME/.local/bin"
+SHARE_DIR="$HOME/.local/share/csd"
 
 # echo "$OS $ARCH"
 
 if [[ "$OS" == "Linux" && "$ARCH" == "x86_64" ]]; then
   echo "Downloading csd from CSD repository..."
   mkdir -p "$BIN_DIR"
+  mkdir -p "$SHARE_DIR"
   
   curl -sSfL "https://github.com/Cit-Software-Distribution/csd-tools/releases/latest/download/csd-x86_64-linux.tar.gz" | tar -xzf - -C "$BIN_DIR"
+  curl -sSfL "https://raw.githubusercontent.com/Cit-Software-Distribution/.github/refs/heads/main/manifest.json" -o "$SHARE_DIR/manifest.json"
   
   chmod +x "$BIN_DIR/csd"
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use clap::{Parser, Subcommand};
+use dirs;
+use serde::{Deserialize, Serialize};
+use serde_json::{self, json};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
+use std::process;
 
 #[derive(Subcommand)]
 enum Commands {
-    Install,
+    Install { package: String },
 }
 
 #[derive(Parser)]
@@ -12,10 +19,38 @@ struct Args {
     command: Commands,
 }
 
-fn main() {
+#[derive(Serialize, Deserialize)]
+struct Package {
+    version: String,
+    url: String,
+    checksum: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Manifest {
+    programs: HashMap<String, Package>,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
+    let mut dir = dirs::data_local_dir().unwrap_or_else(|| {
+        eprintln!("Couldn't find local DATA path.");
+        process::exit(1);
+    });
+
+    dir.push("csd/manifest.json");
+
+    let content = fs::read_to_string(dir)?;
+    let manifest: Manifest = serde_json::from_str(&content)?;
 
     match &args.command {
-        Commands::Install => println!("Buscar pelo pacote no manifesto"),
+        Commands::Install { package } => {
+            if let Some(p) = manifest.programs.get(package) {
+                println!("Package {package:?} was found");
+            } else {
+                println!("Package {package:?} wasn't found");
+            }
+        }
     }
+    Ok(())
 }
