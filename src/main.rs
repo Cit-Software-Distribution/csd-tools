c3ee221cb6ea343eed1687b88abfa2afbce46f89
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-06-23 01:28:57 -0300

    add: adicionar comandos CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Commands {
+    Install,
+}
+
+#[derive(Parser)]
+#[command(name = "csd", author, version, about = "CSD Tooling Ecosystem")]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
 fn main() {
-    println!("Olá mundo");
+    let args = Args::parse();
+
+    match &args.command {
+        Commands::Install => println!("Buscar pelo pacote no manifesto"),
+    }
 }
